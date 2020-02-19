5bcabeecd29ec5364282f5bfafde413d4f1ce228
Leo <leoschreuders@hotmail.com>
2020-02-19 15:59:07 +0100

    findEdge werkt, maar levert nog wel dubbele edges op

--- week3/team/solution3.pl
+++ week3/team/solution3.pl
@@ -1,19 +1,26 @@
 :- consult('route.pl').
 
-edge(From at Time1, To at Time2, Cost) :-
-  findEdge(From, To, Cost, route(_)),
-  write(route),
-  nl().
+edge(From, To, Cost) :-
+  route(Route),
+  findEdge(From, To, Cost, Route).
 
 
-findEdge( From, To, Cost, [X, Y | T]) :-
-  X = From at Time1,
-  Y = To at Time2,
-  Cost = diffTime(Time1, Time2);
-  findedge( From, To, Cost, [Y |T]).
+findEdge(From, To, Cost, Route) :-
+  Route = [F, S | T],
+  (
+  (
+  F = From at Time1,
+  S = To at Time2,
+  diffTime(Time1, Time2, Cost)
+  );
+  findEdge(From, To, Cost, [S|T])
+  ).
 
 
 
 
 
-diffTime(H1:M1, H0:M0, Minutes) :- Minutes is (H1-H0) * 60 + (M1-M0).
+
+
+
+diffTime(H1:M1, H2:M2, Minutes) :- Minutes is (H2*60 + M2) - (H1* 60 + M1).
