f925cdf88226a5dd4d12d3c71aeadc5742007424
Leo <leoschreuders@hotmail.com>
2020-02-19 13:55:10 +0100

    in bestandjes gezet

---
+++ week3/team/solution1.pl
@@ -0,0 +1,14 @@
+:- consult('graph.pl').
+
+path(X, Y, Path) :-
+  path1(X, Y, [], Path).
+
+
+path1(From, To, Visited, Path) :-
+  edge(From, X, Cost),
+  not(member(edge(_, X, _), Visited)),
+  NewVisited = [edge(From, X, Cost) | Visited],
+  (
+    (X = To, reverse(NewVisited, Path) ) ;
+    ( X \= To, path1(X, To, NewVisited, Path))
+  ).
