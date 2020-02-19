0799470a595d3011cdfd6335c8efff22abd3790f
Leo <leoschreuders@hotmail.com>
2020-02-19 16:15:18 +0100

    findEdges werkt nog beter

--- week3/team/solution3.pl
+++ week3/team/solution3.pl
@@ -5,15 +5,15 @@ edge(From, To, Cost) :-
   findEdge(From, To, Cost, Route).
 
 
-findEdge(From, To, Cost, Route) :-
+findEdge(From at Time1, To at Time2, Cost, Route) :-
   Route = [F, S | T],
   (
   (
   F = From at Time1,
   S = To at Time2,
-  diffTime(Time1, Time2, Cost)
+  diffTime(Time2, Time1, Cost)
   );
-  findEdge(From, To, Cost, [S|T])
+  findEdge(From at Time1, To at Time2, Cost, [S|T])
   ).
 
 
@@ -23,4 +23,4 @@ findEdge(From, To, Cost, Route) :-
 
 
 
-diffTime(H1:M1, H2:M2, Minutes) :- Minutes is (H2*60 + M2) - (H1* 60 + M1).
+diffTime(H2:M2, H1:M1, Minutes) :- Minutes is (H2*60 + M2) - (H1* 60 + M1).
