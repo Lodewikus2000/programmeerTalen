a6407332e1451d50c91e7a832c317262a81c3e5d
Lodewikus2000 <leoschreuders@hotmail.com>
2020-03-07 21:47:54 +0100

    passed all local tests

--- week5-erlang/team/rooms.erl
+++ week5-erlang/team/rooms.erl
@@ -13,18 +13,18 @@
 -export([init/1, start_link/0, start_link/1, restart/0, restart/3,
          handle_call/3, handle_cast/2, terminate/2, code_change/3]).
 
--export([add_wall/4, choose_random_wall/1, build_random_wall/1,
-         get_all_walls/2, get_cell_walls/2,
-         get_grid/0, get_open_spots/1, get_wall/3, has_wall/4, new_grid/2,
-         print_grid/1, show_hlines/2, show_vlines/2  ]).
+-export([add_wall/4, build_wall/1, choose_random_wall/1, build_random_wall/1,
+         get_all_walls/2, get_cell_walls/2, get_completable_wall/1,
+         get_completable_walls/1, get_grid/0, get_open_cell_walls/3,
+         get_open_spots/1, get_wall/3, has_wall/4, new_grid/2, print_grid/1,
+         show_hlines/2, show_vlines/2]).
 
- % Starts with an empty board.
+% Starts with an empty board. Default board is 6 x 6.
 start_link() ->
     start_link({6, 6, []}).
 
-% roster is 6 x 6
 
- % Starts with a preconfigured board.
+% Start a gen_server with a preconfigured board.
 start_link(Rooms) ->
     gen_server:start_link({local, rrr}, rooms, Rooms, []).
 
@@ -58,6 +58,17 @@ build_random_wall(Grid) ->
     end.
 
 
+build_wall(Grid) ->
+    Completable_wall = get_completable_wall(Grid),
+    if
+        Completable_wall =:= [] ->
+            build_random_wall(Grid);
+        true ->
+            {M, N, List} = Grid,
+            {M, N, List ++ [Completable_wall]}
+        end.
+
+
 choose_random_wall(Grid) ->
   Open_spots = get_open_spots(Grid),
   Length = length(Open_spots),
@@ -70,6 +81,57 @@ choose_random_wall(Grid) ->
     end.
 
 
+get_all_walls(W, H) ->
+  W_range = lists:seq(0, W-1),
+  H_range = lists:seq(0, H-1),
+  Listlist = [get_cell_walls(X, Y) || X <- W_range, Y <- H_range],
+  List = lists:merge(Listlist),
+  remove_dup(List).
+
+
+get_completable_wall(Grid) ->
+    All_completable_walls = get_completable_walls(Grid),
+    Length = length(All_completable_walls),
+    if
+        Length =:= 0 ->
+            [];
+        true ->
+            Random = rand:uniform(Length),
+            lists:nth(Random, All_completable_walls)
+        end.
+
+
+get_completable_walls(Grid) ->
+    {M, N, List} = Grid,
+    Walls = [get_open_cell_walls(X, Y, Grid) ||
+                            X <- lists:seq(0, M-1),
+                            Y <- lists:seq(0, N-1),
+                            length(get_open_cell_walls(X, Y, Grid)) =:= 1],
+    lists:flatten(Walls).
+
+
+get_cell_walls(X,Y) ->
+    [get_wall(X, Y, Dir) || Dir <- [north, east, south, west]].
+
+
+get_grid() -> gen_server:call(rrr, read).
+
+
+get_open_cell_walls(X, Y, Grid) ->
+    {_, _, List} = Grid,
+    All_cell_walls = get_cell_walls(X, Y),
+    if
+      List =:= [] ->
+        All_cell_walls;
+      true ->
+        lists:subtract(All_cell_walls, List)
+      end.
+
+
+  get_open_spots(Grid) ->
+      {M, N, List} = Grid,
+      All_walls = get_all_walls(M, N),
+      lists:subtract(All_walls, List).
 
 
 get_wall(X, Y, Dir) ->
@@ -82,6 +144,17 @@ get_wall(X, Y, Dir) ->
     end.
 
 
+% Represent a wall position by either a wall "--" or no wall "  ".
+h_line(T, List) ->
+    Member = member(T, List),
+    case Member of
+      false ->
+        "  ";
+      true ->
+        "--"
+      end.
+
+
 has_wall(X, Y, Dir, {_, _, List}) ->
     Get = get_wall(X,Y,Dir),
     case Get of
@@ -92,27 +165,18 @@ has_wall(X, Y, Dir, {_, _, List}) ->
       end.
 
 
-
-
-
-
-
-
 new_grid(Width,Height) ->
     {Width, Height, []}.
 
 
-get_grid() -> gen_server:call(rrr, read).
+print_grid(Grid) ->
+    {_, N, _} = Grid,
+    Range = lists:seq(0, N),
+    String_list = [show_hlines(X, Grid) ++ show_vlines(X, Grid) || X <- Range],
+    Intro = "~nGame board:~n~n",
+    String = join_strings([Intro] ++ String_list),
+    io:format(String).
 
-get_cell_walls(X,Y) ->
-    [get_wall(X, Y, Dir) || Dir <- [north, east, south, west]].
-
-get_all_walls(W, H) ->
-  W_range = lists:seq(0, W-1),
-  H_range = lists:seq(0, H-1),
-  Listlist = [get_cell_walls(X, Y) || X <- W_range, Y <- H_range],
-  List = lists:merge(Listlist),
-  remove_dup(List).
 
 remove_dup([]) -> [];
 remove_dup([H|T]) ->
@@ -124,10 +188,6 @@ remove_dup([H|T]) ->
         [H] ++ remove_dup(T)
       end.
 
-get_open_spots(Grid) ->
-    {M, N, List} = Grid,
-    All_walls = get_all_walls(M, N),
-    lists:subtract(All_walls, List).
 
 v_line(T, List, M) ->
   {{_, _},{X, _}} = T,
@@ -151,30 +211,12 @@ show_vlines(Row, {M, _, List}) ->
     join_strings(String_list).
 
 
-h_line(T, List) ->
-    Member = member(T, List),
-    case Member of
-      false ->
-        "  ";
-      true ->
-        "--"
-      end.
-
-
-% Prints the row ABOVE the given row
+% Prints the row of horizontal wall positions above the given row.
 show_hlines(Row, {M, _, List}) ->
     Range = lists:seq(0, M-1),
-    String_list = ["+"] ++ [ h_line({{X, Row - 1},{X, Row}}, List) ++ "+" || X <- Range ] ++ ["~n"],
+    String_list = ["+"] ++ [ h_line({{X, Row - 1},{X, Row}}, List) ++ "+"
+                             || X <- Range ] ++ ["~n"],
     join_strings(String_list).
-
-
-print_grid(Grid) ->
-    {_, N, _} = Grid,
-    Range = lists:seq(0, N),
-    String_list = [show_hlines(X, Grid) ++ show_vlines(X, Grid) || X <- Range],
-    Intro = "~nGame board:~n~n",
-    String = join_strings([Intro] ++ String_list),
-    io:format(String).
 
 
 join_strings([]) -> [];
