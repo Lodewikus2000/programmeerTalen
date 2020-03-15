3c9fd4532ee73afde3c634c41e03cc81e55c055e
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2020-03-15 14:19:39 +0100

    geprobeerd iets te doen

--- week6-go/team/maze.go
+++ week6-go/team/maze.go
@@ -4,10 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	// "sync"
+	"sync"
     "errors"
 )
 
+const noWall byte = (0 << 0) // The first flag
 const southWall byte = (1 << 0) // The first flag
 const eastWall byte = (1 << 1)  // The second flag
 
@@ -43,40 +44,102 @@ func readMaze(f *os.File) (maze Maze, err error) {
 	return maze, nil
 }
 
-func solve(maze Maze) (route []Position) {
+func solve(maze Maze, goal Position) (route []Position) {
+
 	fmt.Println(maze)
 
-	// Receive messages from the goroutines and spawn new ones as needed
-	// Do not spawn new goroutines if a way out of the maze has been found
-	// Stop receiving only when no more exploration goroutines are running
+	// fill once mace with correct amount of zeros??
+	var onceMaze [][]sync.Once = make([][]int, len(maze))
+	for i := range mat {
+	    onceMaze[i] = make([]int, len(maze[0]))
+	}
+
+	// routes
+	routes := make(chan []Position)
+
+	// start the exploration at {0, 0}
+	var explorestart [1]Position = Position{0, 0}
+
+	// add the first route to the stack
+	routes <- explorestart
+
+	for {
+
+		// gewandeld = matrix van dezelfde grootte als maze, maar met overal een 0
+	    // (dat betekent: nog niet in dit hokje geweest).
+	    // gewandeld[0,0] = 1 (dus in dit hokje zijn we wel geweest)
+	    // locatie = (0,0)
+
+		// Get the route to further explore
+		route := <- routes
+
+		// get the last explored coordinate
+		lastexplored := route[len(route) - 1]
+
+		// stop if we found the goal
+		if lastexplored == goal {
+			return route
+		}
+
+		richtingen := make(chan Position)
+		go step(onceMaze, lastexplored, richtingen)
+
+		for richting := range richtingen {
+			routes <- append(route, richting)
+	    }
+	}
+
+    // while de stack niet leeg is/ het kanaal open is:
+    //     current gewandeld, current_locatie = neem de bovenste item van de stack
+    //     richtingen = check_step(maze, current_locatie)
+	//
+    //     for elke richting in richtingen:
+    //         if gewandeld[richting] == 0:
+    //             nieuw_gewandeld = kopie(current_gewandeld)
+    //             nieuw_gewandeld[richting] = 1
+	//
+    //             if richting == doel_locatie (nog even vaststellen hoe):
+    //                 return nieuw_gewandeld (dit is je route)
+    //             else:
+    //                 voeg achteraan de stack toe {nieuw_gewandeld, richting
 
     return route
 }
 
-func step(maze Maze, position Position) {
-	richtingen := make(chan []Position)
+func step(maze Maze, position Position, richtingen chan Position) {
 
-	row int = Position.Row
-	col int = Position.Col
-	if maze[row, col] == 0:
-        richtingen <- Position{row + 1, col} // we kunnen naar zuid
-        richtingen <- Position{row, col + 1} // we kunnen naar oost
-    else if maze[row, col] == 1:
-        richtingen <- Position{row, col + 1} // we kunnen naar oost
-    else if maze[row, col] == 2:
-        richtingen <- Position{row + 1, col} // we kunnen naar zuid
+	row := position.Row
+	col := position.Col
 
-    if maze[row - 1, col] == 0 of 2:
-        richtingen <- Position{row - 1, col} // we kunnen naar noord
-    if maze[row, col -1] == 0 of 1:
-        richtingen <- Position{row, col - 1} // we kunnen naar west
+	// Add to possible direction to the channel
+	switch maze[row][col] {
+
+		case noWall:
+			richtingen <- Position{row + 1, col}
+	        richtingen <- Position{row, col + 1}
+
+		case southWall:
+			richtingen <- Position{row, col + 1}
+
+		case eastWall:
+			richtingen <- Position{row + 1, col}
+
+		// there are no other cases (so no default required)
+	}
+
+    // if maze[row - 1, col] == 0 or maze[row -1, col] == 2 {
+	// 	richtingen <- Position{row - 1, col} // we kunnen naar noord
+	// }
+    // else if maze[row, col -1] == 0 or maze[row, col - 1] == 1 {
+	// 	richtingen <- Position{row, col - 1} // we kunnen naar west
+	// }
 
 	close(richtingen)
-	return richtingen
+	return
 }
 
 func main() {
-	// TODO: handle errors
+
     if len(os.Args) != 2 {
         fmt.Println("incorrect amount of arguments.")
         os.Exit(1)
@@ -96,7 +159,9 @@ func main() {
 		os.Exit(3)
 	}
 
-	for _, pos := range solve(maze) {
+	goal := Position {len(maze), len(maze[0])}
+
+	for _, pos := range solve(maze, goal) {
 		maze[pos.Row][pos.Col] |= (1 << 2) // The third flag
 	}
 
