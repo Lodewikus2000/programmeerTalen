88bef0f058a7049d2ef3a68c85dd082c1ce73222
Lodewikus2000 <leoschreuders@hotmail.com>
2020-03-23 20:56:56 +0100

    matrix af

--- week7-cpp/evaluator/Matrix.h
+++ week7-cpp/evaluator/Matrix.h
@@ -33,13 +33,10 @@ class Matrix
 /*! Reads a Matrix from 'is' stream. */
 std::istream& operator>>(std::istream& is,Matrix& matrix)
 {
-
     int rows = 0;
     float fl;
 
-
     std::vector<double> data;
-
 
     while (is.peek() != EOF) {
         if (is.peek() == '\n'){
@@ -53,7 +50,6 @@ std::istream& operator>>(std::istream& is,Matrix& matrix)
             // std::cout << fl <<"--";
             data.push_back(fl);
         }
-
     }
     // std::cout << "--"<< rows <<" rows\n";
 
@@ -63,18 +59,14 @@ std::istream& operator>>(std::istream& is,Matrix& matrix)
     matrix.m_cols = cols;
     matrix.m_data = data;
 
-
-
     return is; // to be completed
 }
 
 /*! Writes Matrix 'matrix' to 'os' stream. */
 std::ostream& operator<<(std::ostream& os,const Matrix& matrix)
 {
-
     int rows = matrix.nr_rows();
     int cols = matrix.nr_cols();
-
 
     os << "\n";
     for (int i = 0; i < rows; i++) {
@@ -89,37 +81,112 @@ std::ostream& operator<<(std::ostream& os,const Matrix& matrix)
         }
     }
 
-    return os; // to be completed
+    return os;
 }
 
 /*! Returns a new Matrix that is the negation of 'matrix' */
 Matrix operator-(const Matrix& matrix)
 {
-    return matrix; // to be completed
+    int rows = matrix.nr_rows();
+    int cols = matrix.nr_cols();
+
+    std::vector<double> new_data;
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            new_data.push_back(-matrix(i, j));
+        }
+    }
+    Matrix new_matrix(rows, cols);
+    new_matrix.vec() = new_data;
+
+    return new_matrix;
 }
 
 /*! Returns a new Matrix that is the transpose of 'matrix' */
 Matrix transpose(const Matrix& matrix)
 {
-    return matrix; // to be completed
+    int rows = matrix.nr_rows();
+    int cols = matrix.nr_cols();
+
+    std::vector<double> new_data;
+
+
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            new_data.push_back(matrix(i, j));
+        }
+    }
+    Matrix new_matrix(cols, rows);
+    new_matrix.vec() = new_data;
+
+    return new_matrix;
 }
 
 /*! Returns a new Matrix that is equal to 'm1+m2'. */
 Matrix operator+(const Matrix& m1,const Matrix& m2)
 {
-    return m1; // to be completed
+    int rows = m1.nr_rows();
+    int cols = m1.nr_cols();
+
+    std::vector<double> new_data;
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            new_data.push_back(m1(i, j) + m2(i, j));
+        }
+    }
+    Matrix new_matrix(rows, cols);
+    new_matrix.vec() = new_data;
+
+    return new_matrix;
 }
 
 /*! Returns a new Matrix that is equal to 'm1-m2'. */
 Matrix operator-(const Matrix& m1,const Matrix& m2)
 {
-    return m1; // to be completed
+    int rows = m1.nr_rows();
+    int cols = m1.nr_cols();
+
+    std::vector<double> new_data;
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            new_data.push_back(m1(i, j) - m2(i, j));
+        }
+    }
+    Matrix new_matrix(rows, cols);
+    new_matrix.vec() = new_data;
+
+    return new_matrix;
 }
 
 /*! Returns a new Matrix that is equal to 'm1*m2'. */
 Matrix operator*(const Matrix& m1,const Matrix& m2)
 {
-    return m1; // to be completed
+    int m = m1.nr_rows();
+    int n = m1.nr_cols();
+    int p = m2.nr_cols();
+
+    std::vector<double> new_data;
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            int sum = 0;
+            for (int r = 0; r < n; r++) {
+                sum += m1(i, r) * m2(r, j);
+            }
+            new_data.push_back(sum);
+        }
+    }
+    Matrix new_matrix(m, p);
+    new_matrix.vec() = new_data;
+
+    return new_matrix;
 }
 
 #endif
